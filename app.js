9dad583d8e497f8dd91f582f4d6adeba9d434019
Sushil <sjsushil09@gmail.com>
2023-06-06 21:01:53 +0530

    Introduction to React

---
+++ app.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+//Js method
+
+    // const divRoot = document.getElementById('root');
+    // const h1 = document.createElement('h1');
+    // h1.innerHTML='Hello from JavaScript';
+    // root.appendChild(h1);
+
+
+    //React way of manipulating DOM
+    // const header = React.createElement('h1',{id:"title"},'this is via react'); //header is object
+    // const root = ReactDOM.createRoot(divRoot);//overrides all 
+
+    // root.render(header);
+
+    // const divRoot = document.getElementById('root2');
+    // const header1 = React.createElement('h1',{key:"first"},'First Heading');
+    // const header2 = React.createElement('h2',{key:"second"},'Second Heading');
+    // const divContainer = React.createElement('div',{key:"Test World"},[header1,header2]);
+    
+
+    // const root= ReactDOM.createRoot(divRoot);
+    // root.render(divContainer);
